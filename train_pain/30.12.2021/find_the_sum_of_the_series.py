9330a4b563d077b601c873ce6ec66924d81d9ff2
Aliaksandr Karmyzau <aliaksandr.karmyzau@leverx.com>
2022-01-14 14:47:47 +0300

    Train Pain

---
+++ train_pain/30.12.2021/find_the_sum_of_the_series.py
@@ -0,0 +1,8 @@
+a = 2
+n = 5
+b = 0
+result = 0
+for i in range(1, n + 1):
+    b = b * 10 + a
+    result = result + b
+print(result)
--- train_pain/30.12.2021/reverse_an_integer_number.py
+++ train_pain/30.12.2021/reverse_an_integer_number.py
@@ -1,3 +1,9 @@
 number = int(input())
-7509
-
+# 7509
+# 9057
+result = 0
+while number > 0:
+    a = number % 10
+    number = number // 10
+    result = result * 10 + a
+print(result)
